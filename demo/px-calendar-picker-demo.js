1639fb932f0c398c54ab18553c4c158600a70177
Justin Kirkwood <justin@corvuscreative.ca>
2019-11-27 15:38:31 -0800

    Upgrade from Polymer 2 to 3 via Polymer CLI Modulizer
    Utilized the Polymer CLI Modulizer to convert to javascript imports for Polymer 3

--- demo/px-calendar-picker-demo.js
+++ demo/px-calendar-picker-demo.js
@@ -1,4 +1,4 @@
-<!--
+/*
 Copyright (c) 2018, General Electric
 
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,35 +12,33 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
--->
+*/
+/* Common imports */
+/* Common demo imports */
+/* Imports for this component */
+/* Demo DOM module */
+/*
+  FIXME(polymer-modulizer): the above comments were extracted
+  from HTML and may be out of place here. Review them and
+  then delete this comment!
+*/
+import '@polymer/polymer/polymer-legacy.js';
 
-<!-- Common imports -->
-<link rel="import" href="../../polymer/polymer.html" />
-
-<!-- Common demo imports -->
-<link rel="import" href="../../px-demo/px-demo-header.html" />
-<link rel="import" href="../../px-demo/px-demo-api-viewer.html" />
-<link rel="import" href="../../px-demo/px-demo-footer.html" />
-<link rel="import" href="../../px-demo/px-demo-configs.html" />
-<link rel="import" href="../../px-demo/px-demo-props.html" />
-<link rel="import" href="../../px-demo/px-demo-interactive.html" />
-<link rel="import" href="../../px-demo/px-demo-component-snippet.html" />
-<link rel="import" href="../../px-demo/px-demo-code-editor.html" />
-
-<!-- Imports for this component -->
- 
-<link rel="import" href="../px-calendar-picker.html" />
-
-<!-- Demo DOM module -->
-<dom-module id="px-calendar-picker-demo">
-  <template>
- 
-
+import 'px-demo/px-demo-header.js';
+import 'px-demo/px-demo-api-viewer.js';
+import 'px-demo/px-demo-footer.js';
+import 'px-demo/px-demo-configs.js';
+import 'px-demo/px-demo-props.js';
+import 'px-demo/px-demo-interactive.js';
+import 'px-demo/px-demo-component-snippet.js';
+import 'px-demo/px-demo-code-editor.js';
+import '../px-calendar-picker.js';
+import { Polymer } from '@polymer/polymer/lib/legacy/polymer-fn.js';
+import { html } from '@polymer/polymer/lib/utils/html-tag.js';
+Polymer({
+  _template: html`
     <!-- Header -->
-    <px-demo-header
-    module-name="px-calendar-picker"
-    description="The px-calendar-picker component renders a calendar for a given decade, year, or month, and allows users to select a date or range from the calendar. One main use of this component is in the px-range-panel."
-    mobile tablet desktop>
+    <px-demo-header module-name="px-calendar-picker" description="The px-calendar-picker component renders a calendar for a given decade, year, or month, and allows users to select a date or range from the calendar. One main use of this component is in the px-range-panel." mobile="" tablet="" desktop="">
     </px-demo-header>
 
     <!-- Interactive -->
@@ -56,43 +54,27 @@ limitations under the License.
       <!-- Component ---------------------------------------------------------->
       <px-demo-component slot="px-demo-component">
         <template is="dom-if" if="{{props.preventRangeSelection.value}}">
-          <p class=u-mb0>Event fired: <strong>px-calendar-selected</strong></p>
+          <p class="u-mb0">Event fired: <strong>px-calendar-selected</strong></p>
           <p class="zeta u-mt0">See API Reference below for more details</p>
           <p class="u-mb++">Submitted date: <strong>{{props.fromMoment.value}}</strong></p>
 
         </template>
 
         <template is="dom-if" if="{{!props.preventRangeSelection.value}}">
-          <p class=u-mb0>Event fired: <strong>px-calendar-range-selected</strong></p>
+          <p class="u-mb0">Event fired: <strong>px-calendar-range-selected</strong></p>
           <p class="zeta u-mt0">See API Reference below for more details</p>
           <p class="u-mb++">Submitted range: <strong>{{props.fromMoment.value}}</strong> To: <strong>{{props.toMoment.value}}</strong></p>
 
         </template>
 
         <div style="width:210px">
-          <px-calendar-picker
-            from-moment="{{props.fromMoment.value}}"
-            to-moment="{{props.toMoment.value}}"
-            display-mode="{{props.displayMode.value}}"
-            block-future-dates="{{props.blockFutureDates.value}}"
-            block-past-dates="{{props.blockPastDates.value}}"
-            prevent-range-selection="{{props.preventRangeSelection.value}}"
-            hide-next-button="{{props.hideNextButton.value}}"
-            hide-previous-button="{{props.hidePreviousButton.value}}"
-            shrink="{{props.shrink.value}}"
-            min-date="{{props.minDate.value}}"
-            max-date="{{props.maxDate.value}}">
+          <px-calendar-picker from-moment="{{props.fromMoment.value}}" to-moment="{{props.toMoment.value}}" display-mode="{{props.displayMode.value}}" block-future-dates="{{props.blockFutureDates.value}}" block-past-dates="{{props.blockPastDates.value}}" prevent-range-selection="{{props.preventRangeSelection.value}}" hide-next-button="{{props.hideNextButton.value}}" hide-previous-button="{{props.hidePreviousButton.value}}" shrink="{{props.shrink.value}}" min-date="{{props.minDate.value}}" max-date="{{props.maxDate.value}}">
           </px-calendar-picker>
         </div>
       </px-demo-component>
       <!-- END Component ------------------------------------------------------>
 
-      <px-demo-component-snippet
-        slot="px-demo-component-snippet"
-        element-properties="{{props}}"
-        element-name="px-calendar-picker"
-        links-includes="[[linksIncludes]]"
-        suppress-property-values="[[suppressPropertyValues]]">
+      <px-demo-component-snippet slot="px-demo-component-snippet" element-properties="{{props}}" element-name="px-calendar-picker" links-includes="[[linksIncludes]]" suppress-property-values="[[suppressPropertyValues]]">
       </px-demo-component-snippet>
     </px-demo-interactive>
 
@@ -101,12 +83,8 @@ limitations under the License.
 
     <!-- Footer -->
     <px-demo-footer></px-demo-footer>
+`,
 
-  </template>
-</dom-module>
-
-<script>
-Polymer({
   is: 'px-calendar-picker-demo',
 
   properties: {
@@ -266,6 +244,4 @@ Polymer({
   ready: function () {
     this.linksIncludes = ['px-datetime-common/px-polygit-imports-datetime.html', 'px-icon-set/px-icon-set.html', 'px-icon-set/px-icon.html'];
   }
-
 });
-</script>
